60d1fae3ff3cdd31884227ca2c74fe67d76edaa2
Dev. Gaurav Jatt <devgauravjatt@onmail.com>
2023-09-20 13:32:24 +0530

    addd

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import express from "express";
-import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
 import cors from "cors";
 import main from "./Api.js";
 
@@ -7,7 +8,8 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
